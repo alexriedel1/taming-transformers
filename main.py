e9bb41396fb039b6fa0cb52ce556926c49b5bac7
Alexander Riedel <54716527+alexriedel1@users.noreply.github.com>
2022-08-30 10:42:28 +0200

    dir

--- main.py
+++ main.py
@@ -528,7 +528,7 @@ if __name__ == "__main__":
         callbacks_cfg = OmegaConf.merge(default_callbacks_cfg, callbacks_cfg)
         trainer_kwargs["callbacks"] = [instantiate_from_config(callbacks_cfg[k]) for k in callbacks_cfg]
         trainer_kwargs["log_every_n_steps"] = 1
-        print(trainer_kwargs)
+        print(dir(trainer_kwargs["checkpoint_callback"]))
 
         trainer = Trainer.from_argparse_args(trainer_opt, **trainer_kwargs)
 
