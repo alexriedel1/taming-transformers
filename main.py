c30b4e8b6881437be68bb9dc93e464befdd00c2b
Alexander Riedel <54716527+alexriedel1@users.noreply.github.com>
2022-08-30 12:34:21 +0200

    save every 10k steps

--- main.py
+++ main.py
@@ -483,7 +483,7 @@ if __name__ == "__main__":
                 "filename": "{epoch:06}",
                 "verbose": True,
                 #"save_last": True,
-                "every_n_train_steps": 15
+                "every_n_train_steps": 10000
             }
           }
         }
